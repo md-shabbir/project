67ac284ae016aee522323beb542b4bd2f0a6f4cb
shabbir <sbr543@gmail.com>
2016-08-02 15:23:01 +0530

    implemented function to calculate sum

--- sum.py
+++ sum.py
@@ -1,6 +1,10 @@
 
-a = raw_input('Enter first no: ')
-b = raw_input('Enter second no: ')
+def sum(a, b):
+	return (a + b)
 
-print int(a) + int(b)
+
+a = int(raw_input('Enter first no: '))
+b = int(raw_input('Enter second no: '))
+
+print sum(a, b)
 
