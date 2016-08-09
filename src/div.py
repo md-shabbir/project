6daa12c294de46952e825e7663c33b283f54ae74
shabbir <sbr543@gmail.com>
2016-08-09 11:39:53 +0530

    module div.py is created

---
+++ src/div.py
@@ -0,0 +1,13 @@
+##Division of two numbers
+
+def sum(a, b):
+        return (a / b)
+
+
+a = int(raw_input('Enter first no: '))
+b = int(raw_input('Enter second no: '))
+
+print div(a, b)
+
+#end
+
